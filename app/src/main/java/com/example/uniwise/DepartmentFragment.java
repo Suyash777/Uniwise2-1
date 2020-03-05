6e2d41a0e717ee7286ae3532dbb1b802787af63d
Suyash777 <50289222+Suyash777@users.noreply.github.com>
2020-03-06 04:21:21 +0530

    Update DepartmentFragment.java

--- app/src/main/java/com/example/uniwise/DepartmentFragment.java
+++ app/src/main/java/com/example/uniwise/DepartmentFragment.java
@@ -20,6 +20,7 @@ public class DepartmentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_department, container, false);
     }
 
