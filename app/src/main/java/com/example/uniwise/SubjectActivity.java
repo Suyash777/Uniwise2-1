91e1bd4812a3337da5365e27f99568ad9acdd877
Anuja2512 <61413633+Anuja2512@users.noreply.github.com>
2020-03-08 01:28:33 +0530

    Update SubjectActivity.java

--- app/src/main/java/com/example/uniwise/SubjectActivity.java
+++ app/src/main/java/com/example/uniwise/SubjectActivity.java
@@ -1,14 +1,50 @@
 package com.example.uniwise;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class SubjectActivity extends AppCompatActivity {
+
+    String[] ITsubjectList={"JAVA","CS","JS"};
+    String[] COMPsubjectList={"Android","NMA","JSP"};
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject);
+
+        Intent intent=getIntent();
+        String Subject=intent.getStringExtra("ITkey");
+        
+        RecyclerView Subjects = findViewById(R.id.list_subs);
+        Subjects.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+        switch (Subject) {
+            case "IT":
+            Subjects.setAdapter(new SubjectsAdapter(ITsubjectList));
+            break;
+            case "COMP":
+                Subjects.setAdapter(new SubjectsAdapter(COMPsubjectList));
+                break;
+        }
+
     }
+
+
+
+
 }
